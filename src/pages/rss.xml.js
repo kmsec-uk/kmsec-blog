2d06f475d497407f283a6779e9c3107a9a4fd156
kmsec <dev@null>
2023-12-24 22:01:33 +0000

    fix bugs with migrating to 4.0

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -4,14 +4,15 @@ import { SITE } from '../config'
 const allPosts = import.meta.glob('./blog/*{.md,.mdx}', { eager: true })
 const sortedPosts = Object.values(allPosts).sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
 
-export const get = () => rss({
+export async function GET(context){
     // `<title>` field in output xml
-    title: `${SITE.name}`,
+    return rss({
+    title: SITE.name,
     // `<description>` field in output xml
     description: SITE.description,
     // base URL for RSS <item> links
     // SITE will use "site" from your project's astro.config.
-    site: import.meta.env.SITE,
+    site: context.site,
     // list of `<item>`s in output xml
     // simple example: generate items for every md file in /src/pages
     // see "Generating items" section for required frontmatter and advanced use cases
@@ -22,5 +23,5 @@ export const get = () => rss({
         pubDate: item.frontmatter.date,
     })),
     // (optional) inject custom xml
-    customData: `<language>en</language>`,
-});
+    customData: `<language>en</language>`,})
+};
